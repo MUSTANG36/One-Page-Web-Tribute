d7b1e6f8991d06617c5ada22e5d45cc7fa1e06d8
Ricardo <ochor01@pfw.edu>
2021-03-22 14:13:48 -0400

    Relocating Files to Repo

---
+++ HTML PROJECT 1/myscript.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    var x = document.getElementById("showDiv");
+  
+    if (x.style.display == "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+
+
+  }
+  function startFunction(){
+    var x = document.getElementById("showDiv");
+    x.style.display = "none";
+  }
